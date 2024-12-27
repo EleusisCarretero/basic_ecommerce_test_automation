0f22303d558987ceabd79d1ecb93f96b2ad7b979
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2024-12-27 12:12:06 -0600

    feat: inheritage from BasePage class, include dummy test

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,12 @@
+from basic_ecommerce_test_automation.pages.base_pages import BasePage
+from basic_ecommerce_test_automation.utils.tools import YamlManager
 import pytest
 
-class TestPositiveFlows:
+class TestPositiveFlows(BasePage):
+
+    def __init__(self):
+        testing_page = YamlManager.get_yaml_file_data("basic_ecommerce_test_automation/tests/test_inputs/sauce_demo.yaml")
+        super().__init__(testing_page)
 
     def test_valid_login(self):
-        pass
+        print(f"Content from source demo file: {self.testing_page}")
