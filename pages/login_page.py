61206e7c8c4e728230db3a272d58a0666eee614b
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2024-12-27 14:49:44 -0600

    feat: give defaul values for init method

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,4 +6,5 @@ from basic_ecommerce_test_automation.pages.base_pages import BasePage
 
 
 class LoginPage(BasePage):
-    pass
+    def __init__(self, testing_page, logger, browser):
+        super().__init__(testing_page, logger, browser, "https://www.saucedemo.com/", "--start-maximized")
