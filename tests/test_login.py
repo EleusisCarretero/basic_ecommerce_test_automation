e81fb923512bd42411c335b3925432580a1786be
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2024-12-27 14:50:31 -0600

    fix: change structure of setup method; log_page attribut moved

--- tests/test_login.py
+++ tests/test_login.py
@@ -11,9 +11,12 @@ class TestPositiveFlows:
 
     @pytest.fixture(autouse=True)
     def setup(self):
-        self.login_page = LoginPage("E:/11)_Eleusis_Git_Stuf/basic_ecommerce_test_automation/tests/test_inputs/sauce_demo.yaml")
         logger_manager = LoggerManager(active_logs=True, default_log_folder=self.__class__.__name__)
         self.log = logger_manager.get_logger(self.__class__.__name__)
+        self.login_page = LoginPage(
+            "E:/11)_Eleusis_Git_Stuf/basic_ecommerce_test_automation/tests/test_inputs/sauce_demo.yaml",
+            logger_manager=logger_manager,
+            browser="Edge")
 
     
     def test_valid_login(self):
