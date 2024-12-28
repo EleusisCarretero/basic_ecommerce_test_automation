43e57e7376f0f8803c59468d205a1315e5933ede
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2024-12-28 15:07:26 -0600

    feat: including more steps to get the credentials

--- tests/test_login.py
+++ tests/test_login.py
@@ -13,4 +13,10 @@ class TestPositiveFlows:
             "E:/11)_Eleusis_Git_Stuf/basic_ecommerce_test_automation/tests/test_inputs/sauce_demo.yaml")
 
     def test_valid_login(self):
-        self.log.info(f"Content from source demo file: {self.login_page.testing_page}")
+        users = self.login_page.get_valid_credentials()
+        self.log.info(f"Accepted users: {users}")
+        user = users[0]
+        self.login_page.login_page(user["user"], user["password"])
+        alert_text = self.login_page.get_alert_text()
+        self.log.info(f"Alert Text: {alert_text}")
+        self.login_page.acpet_alert()
