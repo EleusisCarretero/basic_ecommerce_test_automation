f9e4a1eaa085e732a6cd4927e96f79ac7bf376d4
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2024-12-28 15:10:56 -0600

    feat: including methods to get text, write text and click on elements

--- pages/base_pages.py
+++ pages/base_pages.py
@@ -1,6 +1,7 @@
 """
 Base test page class
 """
+from selenium.webdriver.common.by import By
 from basic_ecommerce_test_automation.utils.logger_manager import LoggerManager
 
 
@@ -13,7 +14,51 @@ class BasePage:
     def __init__(self, browser):
         self.driver = browser.get_driver()
         self.log = LoggerManager.get_logger(self.__class__.__name__)
+
+    def get_text_element(self, by, element):
+        try:
+            text_element = self.driver.find_element(getattr(By, by), element).text
+        except AttributeError as e:
+            self.log.error("No such attribute")
+            raise BasePageException("Element not having text") from e
+        return text_element
+
+    def set_element_value(self, by, element, value):
+        try:
+            self.driver.find_element(getattr(By, by), element).send_keys(value)
+        except AttributeError as e:
+            self.log.error("No such attribute")
+            raise BasePageException("Element not having text") from e
         
+    def click_on_element(self, by, element):
+        try:
+            self.driver.find_element(getattr(By, by), element).click()
+        except AttributeError as e:
+            self.log.error("No such attribute")
+            raise BasePageException("Element not having text") from e
+        
+    def get_alert_text(self):
+        try:
+            alert = self.driver.switch_to.alert
+        except AttributeError as e:
+            self.log.error("No such attribute")
+            raise BasePageException("Element not having text") from e
+        self.driver.active_element()
+        return alert.text
+    
+    def acpet_alert(self):
+        try:
+            alert = self.driver.switch_to.alert
+        except AttributeError as e:
+            self.log.error("No such attribute")
+            raise BasePageException("Element not having text") from e
+        alert.acept()
+
+    
+    @staticmethod
+    def _convert_text_to_list(text, spliter, ini=0, end=-1):
+        return text.split(spliter)[ini:end]
+
 
     @property
     def testing_page(self):
