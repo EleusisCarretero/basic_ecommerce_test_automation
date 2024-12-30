42b1d9a2dc836907155940542d6b418265c1854c
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2024-12-30 01:09:50 -0600

    feat& fix: include a common class to inheritate for the rest of classes and include a test for login and out

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,27 +1,53 @@
 """
-Contants all login test cases
+Contains all login test cases
 """
 import pytest
+from selenium.common.exceptions import ElementClickInterceptedException, InvalidElementStateException, NoSuchElementException
+from basic_ecommerce_test_automation.pages.home_page import HomePage
 from basic_ecommerce_test_automation.pages.login_page import LoginPage
-from basic_ecommerce_test_automation.utils.logger_manager import LoggerManager
+from basic_ecommerce_test_automation.tests.base_test import BaseTest
 from basic_ecommerce_test_automation.utils.tools import YamlManager
 
 
-class TestPositiveFlows:
-    """
-    Test class to validate positive login flows
-    """
+class BaseLogIn(BaseTest):
     TESTING_PAGE =  "E:/11)_Eleusis_Git_Stuf/basic_ecommerce_test_automation/tests/test_inputs/sauce_demo.yaml"
-
-    @pytest.fixture(autouse=True)
     def setup(self, browser, result):
+        super().setup(browser, result)
         self.inventory_page_dict = YamlManager.get_yaml_file_data(self.TESTING_PAGE)["general_inputs"]["inventory_page"]
-        self.log = LoggerManager.get_logger(self.__class__.__name__)
-        self.result = result
         self.login_page = LoginPage(
             browser,
             self.TESTING_PAGE
         )
+        self.home_page = HomePage(
+            browser,
+            self.TESTING_PAGE
+        )
+
+    def step_check_login_successfully(self, user_credential:dict) -> None:
+        """
+        Step function to validate the correct login.
+
+        Args:
+            user_credential (dict): username: password, credentials to login.
+        """
+        step_msg = f"Check using credentials {user_credential} we are able to login successfully."
+        self.result.check_not_raises_any_exception(
+            self.login_page.login_page,
+              step_msg,
+              **user_credential
+        )
+        assert self.result.step_status
+
+
+class TestPositiveFlows(BaseLogIn):
+    """
+    Test class to validate positive login flows
+    """
+    
+    @pytest.fixture(autouse=True)
+    def setup(self, browser, result):
+        super().setup(browser, result)
+        self.login_page.open_page()
 
     def test_valid_login(self):
         """
@@ -30,13 +56,7 @@ class TestPositiveFlows:
         # 1. Get the user credentials.
         user_credential = self.login_page.get_just_specific_user("standard_user")
         # 2. Check if we are able to login without exceptions.
-        step_msg = f"Check using credentials {user_credential} we are able to login successfully."
-        self.result.check_not_raises_any_exception(
-            self.login_page.login_page,
-              step_msg,
-              **user_credential
-        )
-        assert self.result.step_status
+        self.step_check_login_successfully(user_credential)
         # 3. Check the url correspond to the inventory url
         inventory_path = f"{self.login_page.testing_page}{self.inventory_page_dict['path']}"
         stp_msg = "Check the expected inventory path matches with the current url from opened window."
@@ -44,5 +64,28 @@ class TestPositiveFlows:
             self.login_page.get_current_url(True),
             inventory_path,
             stp_msg
+        )
+        assert self.result.step_status
+
+    def test_login_logout(self):
+        """
+        
+        """
+        # 1. Get the user credentials.
+        user_credential = self.login_page.get_just_specific_user("standard_user")
+        # 2. Check if we are able to login without exceptions.
+        self.step_check_login_successfully(user_credential)
+        #3. click on lateral menu
+        self.result.check_not_raises_any_given_exception(
+            method=self.home_page.click_on_lateral_menu,
+            exceptions=(InvalidElementStateException, ElementClickInterceptedException, NoSuchElementException),
+            step_msg=" Check lateral menu is clickable"
+        )
+        assert self.result.step_status
+        # 4. Click on logout
+        self.result.check_not_raises_any_given_exception(
+            method=self.home_page.click_on_log_out,
+            exceptions=(InvalidElementStateException, ElementClickInterceptedException, NoSuchElementException),
+            step_msg="Check Logout successfully"
         )
         assert self.result.step_status
