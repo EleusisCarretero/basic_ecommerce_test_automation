67f8d3134561eb44d64f84c8e1d04a5e48c47e6e
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2024-12-30 01:12:28 -0600

    fix: use of browser manager intance for control elemetes istead of directly use driver

--- pages/base_pages.py
+++ pages/base_pages.py
@@ -11,31 +11,47 @@ class BasePageException(Exception):
 
 class BasePage:
 
+    page_dict = {}
+
     def __init__(self, browser):
         self.driver = browser.get_driver()
+        self.browser = browser
         self.log = LoggerManager.get_logger(self.__class__.__name__)
 
-    def get_text_element(self, by, element):
-        try:
-            text_element = self.driver.find_element(getattr(By, by), element).text
-        except AttributeError as e:
-            self.log.error("No such attribute")
-            raise BasePageException("Element not having text") from e
-        return text_element
+    @property
+    def testing_page(self):
+        return self._testing_page
+    
+    @testing_page.setter
+    def testing_page(self, new_value):
+        self._testing_page = new_value
 
-    def set_element_value(self, by, element, value):
-        try:
-            self.driver.find_element(getattr(By, by), element).send_keys(value)
-        except AttributeError as e:
-            self.log.error("No such attribute")
-            raise BasePageException("Element not having text") from e
+    def _get_element_params(self, key):
+        by = self.page_dict[key]["by"]
+        ele = self.page_dict[key]["value"]
+        return by, ele
+
+    def open_page(self):
+        """
+        Open browser page give the value from 'testing_page'
+        """
+        self.browser.open_page(self.testing_page)
+    
+    def _get_webdriver_element_obj(self, by, value, driver=None, timeout=5):
+        driver = driver or self.browser.driver
+        return self.browser.get_web_element(by=by, value=value, driver=driver, timeout=timeout)
         
-    def click_on_element(self, by, element):
-        try:
-            self.driver.find_element(getattr(By, by), element).click()
-        except AttributeError as e:
-            self.log.error("No such attribute")
-            raise BasePageException("Element not having text") from e
+    def click_on_element(self, by, value, driver=None):
+        driver = driver or self.browser.driver
+        self.browser.click_located_element(by=by, value=value, driver=driver)
+    
+    def get_text_element(self, by, value, driver=None):
+        driver = driver or self.browser.driver
+        return self.browser.get_text_from_element(by=by, value=value, driver=driver)
+
+    def set_element_value(self, by, value, keys_value, driver=None):
+        driver = driver or self.browser.driver
+        self.browser.enter_text_to_element(by=by, value=value, driver=driver, keys_value=keys_value)
         
     def get_alert_text(self):
         try:
@@ -54,16 +70,15 @@ class BasePage:
             raise BasePageException("Element not having text") from e
         alert.acept()
 
+    def get_current_url(self, switch=False):
+        if switch:
+            windows = self.driver.window_handles
+            self.driver.switch_to.window(windows[0])
+        return self.driver.current_url
+
+
     
     @staticmethod
     def _convert_text_to_list(text, spliter, ini=0, end=-1):
         return text.split(spliter)[ini:end]
 
-
-    @property
-    def testing_page(self):
-        return self._testing_page
-    
-    @testing_page.setter
-    def testing_page(self, new_value):
-        self._testing_page = new_value
