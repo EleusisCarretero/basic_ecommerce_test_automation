fd71f158e7df426ff47c1c134294c981b4d0ce94
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2024-12-30 03:04:06 -0600

    fix&docs: change the way the methods calls the driver methods, remove the use of the decorator, include docstrings

--- utils/browser_manager.py
+++ utils/browser_manager.py
@@ -1,8 +1,9 @@
 """
 Browser manager class
 """
+from typing import Union
 from basic_ecommerce_test_automation.utils.logger_manager import LoggerManager
-from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import ElementClickInterceptedException, TimeoutException, NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
@@ -17,6 +18,9 @@ class AvailableBrowsers(str, Enum):
 
     @classmethod
     def get_available_browsers(cls):
+        """
+        Returns a list with al class elements.
+        """
         return [browser for browser in cls]
 
 
@@ -45,17 +49,24 @@ def wait_for_element():
 
 
 class BrowserManagerException(Exception):
+    """BrowserManager Exception class"""
     pass
 
 
 class BrowserManager:
+    """
+    Class to manage with the webdriver instance.
 
-    def __init__(self, browser=AvailableBrowsers.CHROME, url=None, *args):
+    Attributes:
+        log (logger): Logger instance.
+        driver(Webdriver): Webdriver instance.
+    """
+
+    def __init__(self, *args, browser=AvailableBrowsers.CHROME, url=None):
         self.log = LoggerManager.get_logger(self.__class__.__name__)
-        self.driver = self._init_webdriver(browser, *args)
+        self._driver = self._init_webdriver(browser, *args)
         if url:
             self.open_page(url)
-
 
     def _init_webdriver(self, browser, *args):
         if browser not in AvailableBrowsers.get_available_browsers():
@@ -70,8 +81,30 @@ class BrowserManager:
             self.log.error(f"Error webdriver does not have attribute: {browser}")
             raise BrowserManagerException(f"Error webdriver does not have attribute: {browser}") from e
         return driver
+    
+    @property
+    def driver(self):
+        return self._driver
+    
+    @driver.setter
+    def driver(self, new_driver):
+        self._driver = new_driver
 
-    def open_page(self, url):
+    # def get_driver(self) -> None:
+    #     return self.driver
+
+    def open_page(self, url:str, driver=None) -> None:
+        """
+        Opens the url from web page.
+
+        Args:
+            url(str): url from webpage.
+            driver:(webdriver obj:Optional, Default=None): webdriver object.
+
+        Raises:
+            BrowserManagerException: If driver is unable to open url.
+        """
+        driver = driver or self.driver
         try:
             self.driver.get(url)
             self.log.info(f"Opening page: {url}")
@@ -79,36 +112,173 @@ class BrowserManager:
             self.log.error(f"Exception occurred: {e}")
             raise BrowserManagerException(f"Error trying to set page {url}") from e
 
-    def _get_webdriver_element_obj(self, driver, by, element):
-        return driver.find_element(getattr(By, by), element)
-
-    @wait_for_element()
-    def click_located_element(self, element=None, **kwargs):
+    def get_webdriver_element_obj(self, driver, by, value):
         """
-        Clic en un elemento localizado.
+        Looks for an element, in driver, witch matches with the given by and value.
+
+        Args:
+            by(By): By enum, ID, XPATH, etc.
+            value:(str): pattern to find the element.
+            driver:(webdriver obj:Optional, Default=None): webdriver object.
+
+        Returns:
+            Element: element with matches the specifications.
+
+        Raises:
+            BrowserManagerException: In case there is no element in driver witch matches.
+        """
+        driver = driver or self.driver
+        try:
+            return driver.find_element(getattr(By, by), value)
+        except NoSuchElementException as e:
+            self.log.error(f"Unable to find element with parameters ('{by}', '{value}')")
+            raise BrowserManagerException("Unable to find element") from e
+
+    def click_wait_clickable_element(self, by:By, value:str, driver=None, timeout: Union[int, float]=10):
+        """
+        Waits until the element which matches with the 'by' and ' value' within a timeout and clicks on it, otherwise
+        the BrowserManagerException is raised.
+
+        Args:
+            by(By): By enum, ID, XPATH, etc.
+            value:(str): pattern to find the element.
+            driver:(webdriver obj:Optional, Default=None): webdriver object.
+            timeout: (int/float): Timeout in seconds to wait.
+
+        Raises:
+            BrowserManagerException: In case the timeout has been reached and the element is still not clickable.
+        """
+        driver = driver or self.driver
+        try:
+            element = WebDriverWait(driver, timeout).until(
+                EC.element_to_be_clickable((getattr(By, by), value))
+            )
+        except TimeoutException as e:
+            self.log.error(f"Unable to have element '({by}, {value})' clickable within {timeout}s")
+            raise BrowserManagerException("Unable to get element clickable") from e
+        element.click()
+
+    def get_present_element(self, by:By, value:str, driver, timeout: Union[int, float]):
+        """
+        Waits until the element which matches with the 'by' and ' value' within a timeout, (it does not means it is visible)
+        otherwise the BrowserManagerException is raised.
+
+        Args:
+            by(By): By enum, ID, XPATH, etc.
+            value:(str): pattern to find the element.
+            driver:(webdriver obj:Optional, Default=None): webdriver object.
+            timeout: (int/float): Timeout in seconds to wait.
+
+        Returns:
+            Element: Element found which matches within timeout.
+
+        Raises:
+            BrowserManagerException: In case the timeout has been reached and the element is still not present.
+        """
+        driver = driver or self.driver
+        try:
+            return WebDriverWait(driver, timeout).until(
+                EC.presence_of_element_located((getattr(By, by), value))
+            )
+        except TimeoutException as e:
+            self.log.error(f"Unable get the element using parameters '({by}, {value})' within {timeout}s")
+            raise BrowserManagerException("Unable to write on element") from e
+
+    def get_visible_element(self, by:By, value:str, driver, timeout: Union[int, float]):
+        """
+        Waits until the element which matches with the 'by' and ' value' within a timeout, (the element should be visible)
+        otherwise the BrowserManagerException is raised.
+
+        Args:
+            by(By): By enum, ID, XPATH, etc.
+            value:(str): pattern to find the element.
+            driver:(webdriver obj:Optional, Default=None): webdriver object.
+            timeout: (int/float): Timeout in seconds to wait.
+
+        Returns:
+            Element: Element found which matches within timeout.
+
+        Raises:
+            BrowserManagerException: In case the timeout has been reached and the element is still not visible.
+        """
+        driver = driver or self.driver
+        try:
+            return WebDriverWait(driver, timeout).until(
+                EC.visibility_of_element_located((getattr(By, by), value))
+            )
+        except TimeoutException as e:
+            self.log.error(f"Unable get the element using parameters '({by}, {value})' within {timeout}s")
+            raise BrowserManagerException("Unable to write on element") from e
+
+    def enter_text_to_present_element(self, by:By, value:str, keys_value:str, driver, timeout: Union[int, float]) -> None:
+        """
+        Writes a text ('keys_value') the located element, not necessary visible, with the 'by' and ' value' within a timeout, otherwise
+        the BrowserManagerException is raised.
+
+        Args:
+            by(By): By enum, ID, XPATH, etc.
+            value:(str): pattern to find the element.
+            driver:(webdriver obj:Optional, Default=None): webdriver object.
+            timeout: (int/float): Timeout in seconds to wait.
+        """
+        element = self.get_present_element(by, value, driver, timeout)
+        element.clear()
+        element.send_keys(keys_value)
+
+    def get_element_text(self, by:By, value:str, driver, timeout: Union[int, float], visible=True) -> str:
+        """
+        Gets the text from an element.
+
+        Args:
+            by(By): By enum, ID, XPATH, etc.
+            value:(str): pattern to find the element.
+            driver:(webdriver obj:Optional, Default=None): webdriver object.
+            timeout: (int/float): Timeout in seconds to wait.
+            visible:(bool): Flag to indicate if the element should visible or not
+        
+        Returns:
+            str: Text from element.
+        """
+        if visible:
+            return self.get_visible_element(by, value, driver, timeout).text
+        return self.get_present_element(by, value, driver, timeout).text
+
+    def switch_window(self, which_window:int=0) -> str:
+        """
+        Handle the switching windows
+
+        Args:
+            which_window(int:default=0): position of window to switch.
+
+        Returns:
+            str: current ulr from window.
+
+        Raises:
+            BrowserManagerException: unable to switch window
         """
         try:
-            element.click()
-        except ElementClickInterceptedException as e:
-            self.log.error(f"Element {kwargs.get('value')} is not clickable.")
-            raise BrowserManagerException("Unable to click on element") from e
-
-    @wait_for_element()
-    def get_text_from_element(self, element=None, **kwargs):
-        return element.text
+            windows = self.driver.window_handles
+            self.driver.switch_to.window(windows[which_window])
+        except Exception as e:
+            self.log.error(f"Unable to switch the current driver to window {which_window}")
+            raise BrowserManagerException("Unable to switch window") from e
+        return self.get_current_driver_url()
     
-    @wait_for_element()
-    def enter_text_to_element(self, element=None, **kwargs):
-        element.clear()
-        element.send_keys(kwargs["keys_value"])
+    def get_current_driver_url(self, driver=None) -> str:
+        """
+        Return url from given driver.
 
-        
-    @wait_for_element()
-    def get_web_element(self, element=None, **kwargs):
-        return element
+        Args:
+            driver:(webdriver obj:Optional, Default=None): webdriver object.
 
-    def get_driver(self):
-        return self.driver
-    
-    def driver_down(self):
+        Returns:
+            str: current url from give driver.
+        """
+        driver = driver or self.driver
+        return driver.current_url
+
+    def driver_down(self) -> None:
+        """
+        Teardown driver.
+        """
         self.driver.quit()
