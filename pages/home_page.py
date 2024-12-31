80e4399e7a85ed8bac40d1cdc940d6484bb15d04
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2024-12-31 01:19:39 -0600

    docs: missing docstring

--- pages/home_page.py
+++ pages/home_page.py
@@ -10,6 +10,13 @@ class HomePageException(Exception):
 
 
 class HomePage(BasePage):
+    """
+    Class to manage all the functionalities related to the Home page.
+
+    Attributes:
+        LOGIN_PAGE_DICT (dict): Saves al the needed and/or relevant inputs for Home page
+        testing_page (str): Login page path
+    """
       
     def __init__(self, browser, testing_page):
         super().__init__(browser)
@@ -17,13 +24,30 @@ class HomePage(BasePage):
         self.testing_page = self.page_dict["path"]
 
     def click_on_lateral_menu(self):
+        """
+        Clicks on lateral meu from Home page.
+
+        It calls click_on_element from 'BasePage' to click on an element. _get_element_params
+        returns the need parameters, by and value, to get the correct element.
+        """
         self.click_on_element(
-            *self._get_element_params(key="lateral_menu"))
+            *self._get_element_params(
+                key="lateral_menu"
+            )
+        )
     
     def get_lateral_menu_items(self):
+        """
+        Method to get all the webdriver elements that are part of the lateral menu.
+
+        Returns:
+            Webdriver obj: Returns the webdriver element which contains the lateral menu parts.
+        """
         return self.get_webdriver_element_obj(*self._get_element_params(key="lateral_menu_items"))
     
     def click_on_log_out(self):
+        """
+        Clicks on the logout button, which is part of the lateral menu.
+        """
         all_menu_items = self.get_lateral_menu_items()
         self.click_on_element(*self._get_element_params(key="logout"), all_menu_items)
-
