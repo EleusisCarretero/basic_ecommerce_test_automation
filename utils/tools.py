4c78e97a8896c46157eedf90566bbc422d539842
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2024-12-31 01:35:15 -0600

    docs: missing docstring for tools class

--- utils/tools.py
+++ utils/tools.py
@@ -1,10 +1,18 @@
+"""
+Contains common functions, constants, classes that can be util but not
+necessary is part of a feature.
+"""
 import yaml
 
 
 class YamlManager:
+    """
+    Class which handle with yaml file stuff, as well load info, write, etc
+    """
 
     @staticmethod
     def get_yaml_file_data(file_path):
+        """Return a dictionary with the content from the given 'file_path' parameter"""
         content = {}
         with open(file_path, "r") as file:
             content = yaml.safe_load(file)
