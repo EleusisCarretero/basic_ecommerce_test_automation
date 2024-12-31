85da34558dbb2979be981b6f12509d58548c0658
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2024-12-31 01:52:15 -0600

    feat&docs: new console param to run browser, missing docstrings

--- conftest.py
+++ conftest.py
@@ -9,24 +9,37 @@ from basic_ecommerce_test_automation.utils.result_manager import ResultManagerCl
 
 
 def pytest_addoption(parser):
+    """
+    Defines the costume console inputs to run the tests
+    """
     parser.addoption("--log_folder", action="store", default="Logs", help="Folder to store logs")
+    parser.addoption("--browser_type", action="store", default="Edge", help="Browser to execute the tests")
+
 
 @pytest.fixture(scope="session", autouse=True)
 def configure_logging(pytestconfig):
     """
+    Fixture that setups the common logger.
     """
     log_folder = pytestconfig.getoption("log_folder")
     Config.log_folder = log_folder
     LoggerManager.setup_logger()
 
 
-@pytest.fixture(scope="class", params=["Edge"])
-def browser(request):
-    manager = BrowserManager(browser=request.param)
+@pytest.fixture(scope="class")
+def browser(pytestconfig):
+    """
+    Fixture to setup the instance for BrowserManager common in all test cases.
+    """
+    browser_type =  pytestconfig.getoption("browser_type")
+    manager = BrowserManager(browser=browser_type)
     yield manager
     manager.driver_down()
 
 
 @pytest.fixture(scope="class")
 def result():
+    """
+    Fixture to get the instance of ResultManagerClass, common in all tests.
+    """
     return ResultManagerClass()
