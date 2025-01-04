c658ff8042efba5a416380e8564c7dc3436fab28
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-01-03 22:31:36 -0600

    feat: create page to content product page

---
+++ pages/product_page.py
@@ -0,0 +1,31 @@
+"""
+Login page class
+"""
+from basic_ecommerce_test_automation.pages.base_pages import BasePage
+from basic_ecommerce_test_automation.utils.tools import YamlManager
+
+
+class ProductPageException(Exception):
+    """
+    LoginPase error
+    """
+
+
+class ProductPage(BasePage):
+    """
+    Class to manage all the functionalities related to the login page.
+
+    Attributes:
+        LOGIN_PAGE_DICT (dict): Saves al the needed and/or relevant inputs for login page
+        testing_page (str): Login page path
+    """
+    def __init__(self, browser, testing_page):
+        super().__init__(browser)
+        self.page_dict = YamlManager.get_yaml_file_data(testing_page)["general_inputs"]["product_page"]
+        # self.testing_page = self.page_dict["path"]
+
+    def add_item_to_cart(self):
+        self.click_on_element(*self._get_element_params(key="add_to_cart_button"))
+    
+    def back_to_home_page(self):
+        self.click_on_element(*self._get_element_params(key="back_to_products"))
