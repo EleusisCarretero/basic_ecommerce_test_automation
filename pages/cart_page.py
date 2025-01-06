6bbeb2d8eed32d3d3b1fcbb4e8b218add456e843
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-01-05 22:17:22 -0600

    feat: include the methods to go to checkout and coma back to home page; as well as their keys

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,5 +1,5 @@
 """
-Login page class
+Cart page file related to class
 """
 from basic_ecommerce_test_automation.pages.base_pages import BasePage
 from basic_ecommerce_test_automation.utils.tools import YamlManager
@@ -23,3 +23,15 @@ class CartPage(BasePage):
         super().__init__(browser)
         self.page_dict = YamlManager.get_yaml_file_data(testing_page)["general_inputs"]["cart_page"]
         self.testing_page = self.page_dict["path"]
+
+    def move_to_checkout_page(self):
+        """
+        Method to move from cart page to checkout page
+        """
+        self.click_on_element(*self._get_element_params(key="checkout_button"))
+    
+    def continues_shopping(self):
+        """
+        Method to come back from cart page to home page
+        """
+        self.click_on_element(*self._get_element_params(key="continues_shopping"))
