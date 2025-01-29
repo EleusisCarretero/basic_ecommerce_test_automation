ba89bde03ff9ffeafc74a76a38708a001742b42f
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-01-29 13:14:18 -0600

    feat: include method to initilize api

--- conftest.py
+++ conftest.py
@@ -1,11 +1,17 @@
 """
 General configuration
 """
+import multiprocessing
+import subprocess
+import time
+import requests
 import pytest
 from basic_ecommerce_test_automation.utils.config import Config
 from basic_ecommerce_test_automation.utils.logger_manager import LoggerManager
 from basic_ecommerce_test_automation.utils.browser_manager import BrowserManager
 from basic_ecommerce_test_automation.utils.result_manager import ResultManagerClass
+from basic_ecommerce_test_automation.utils.api import APIMongoDB
+from flask import Flask
 
 
 def pytest_addoption(parser):
@@ -43,3 +49,39 @@ def result():
     Fixture to get the instance of ResultManagerClass, common in all tests.
     """
     return ResultManagerClass()
+
+
+@pytest.fixture(scope="session")
+def run_users_api(api_settings):
+    """
+    Fixture to get the instance of ResultManagerClass, common in all tests.
+    """
+    server = subprocess.Popen(
+        [
+            "python",
+            "E:\\11)_Eleusis_Git_Stuf\\basic_ecommerce_test_automation\\utils\\api.py",
+            "--mongo_uri",
+            api_settings["mongo_uri"],
+            "--db_name",
+            api_settings["db_name"],
+            "--collection_name",
+            api_settings["collection_name"]
+        ],
+        stdout=subprocess.DEVNULL,  # Ignorar salida estándar
+        stderr=subprocess.PIPE,  # Capturar errores
+        shell=True
+        )
+
+    time.sleep(3)
+
+    try:
+        response = requests.get("http://127.0.0.1:5000")
+        if response.status_code != 200:
+            pytest.exit(" La API Flask no se inició correctamente.")
+    except requests.ConnectionError:
+        pytest.exit(" No se pudo conectar a la API Flask.")
+
+    yield 
+
+    server.terminate()
+    server.wait()
