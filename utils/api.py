087b433e31362b36589f5129b8927654bef588d1
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-01-29 00:04:37 -0600

    feat: file to use api which gives the data from db

---
+++ utils/api.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+import argparse
+
+class APIMongoDB:
+
+    def __init__(self, app, mongo_uri, db_name, collection_name):
+        self.app = app
+        self.mongo_uri = mongo_uri
+        self.client = MongoClient(mongo_uri)
+        self.db = self.client[db_name]
+        self.collection = self.db[collection_name]
+        self.add_routes()
+    
+    def add_routes(self):
+        """Register API routs"""
+        self.app.add_url_rule("/", view_func=self.home)
+        self.app.add_url_rule("/users", view_func=self.get_users, methods=["GET"])
+        self.app.add_url_rule("/add_user", view_func=self.add_user, methods=["POST"])
+
+    def home(self):
+        return jsonify({"message": "API Flask with MongoDB"})
+
+    def get_users(self):
+        users = list(self.collection.find({}, {"_id": 0}))  # Excluye _id
+        return jsonify(users)
+
+    def add_user(self):
+        data = request.json
+        self.collection.insert_one(data)
+        return jsonify({"message": "User successfully added"}), 201
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+                    prog='ProgramName',
+                    description='What the program does',
+                    epilog='Text at the bottom of help')
+    parser.add_argument("-m", "--mongo_uri", help="The MongoDB URI", default="mongodb+srv://eleusiscarretero:J8nKfYYIuPa0XcJB@cluster0.73nlx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    parser.add_argument("-d", "--db_name",  help="The Name of the desired database", default="users_data")
+    parser.add_argument("-c", "--collection_name",  help="The Name of the desired collection", default="users")
+    args = parser.parse_args()
+    app = Flask(__name__)
+    md_db = APIMongoDB(app,args.mongo_uri, args.db_name, args.collection_name)
+    app.run(debug=True)
+    
