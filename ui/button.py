d22b511e89d5911d97d418a6443281ca48bea6ed
EleusisCarretero <eleusis.carretero@gmail.com>
2025-09-15 18:44:33 -0600

    feat: modifye click method

--- ui/button.py
+++ ui/button.py
@@ -1,6 +1,10 @@
 
-from ui.base_web_element import BaseWebElement
+from ui.base_web_element import BaseWebElement, BaseWebElementException
 from selenium.webdriver.support import expected_conditions as EC
+
+
+class ButtonException(Exception):
+    pass
 
 
 class Button(BaseWebElement):
@@ -12,8 +16,10 @@ class Button(BaseWebElement):
     def _common_click(self):
         self.move_on(EC.element_to_be_clickable(self._element.locator()))
 
-
     def click(self):
-
-        self._common_click()
-        self.actions.click(self.element).perform()
+        try:
+            self._common_click()
+            self.actions.click(self.element).perform()
+        except BaseWebElementException as e:
+            self.log.error(f"Not clicked because exception: {e}")
+            raise ButtonException("No click on button element") from e
